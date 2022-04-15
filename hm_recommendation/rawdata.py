5aef7e16a2f55b536681bc471fbd02d40f824620
James Corbin <jamesedwcorbin@gmail.com>
2022-04-15 17:25:33 -0500

    checkin 2022-04-15

--- hm_recommendation/rawdata.py
+++ hm_recommendation/rawdata.py
@@ -2,32 +2,34 @@ import sys
 import os
 import pandas as pd
 
+unk = "[unk]"
+
 def get_unknown_article():
     return {
             "product_code": 0,
-            "prod_name": b"",
+            "prod_name": unk.encode("utf-8"),
             "product_type_no": 0,
-            "product_type_name": b"",
-            "product_group_name": b"",
+            "product_type_name": unk.encode("utf-8"),
+            "product_group_name": unk.encode("utf-8"),
             "graphical_appearance_no": 0,
-            "graphical_appearance_name": b"",
+            "graphical_appearance_name": unk.encode("utf-8"),
             "colour_group_code": 0,
-            "colour_group_name": b"",
+            "colour_group_name": unk.encode("utf-8"),
             "perceived_colour_value_id": 0,
-            "perceived_colour_value_name": b"",
+            "perceived_colour_value_name": unk.encode("utf-8"),
             "perceived_colour_master_id": 0,
-            "perceived_colour_master_name": b"",
+            "perceived_colour_master_name": unk.encode("utf-8"),
             "department_no": 0,
-            "department_name": b"",
+            "department_name": unk.encode("utf-8"),
             "index_code": 0,
-            "index_name": b"",
+            "index_name": unk.encode("utf-8"),
             "index_group_no": 0,
-            "index_group_name": b"",
+            "index_group_name": unk.encode("utf-8"),
             "section_no": 0,
-            "section_name": b"",
+            "section_name": unk.encode("utf-8"),
             "garment_group_no": 0,
-            "garment_group_name": b"",
-            "detail_desc": b""}
+            "garment_group_name": unk.encode("utf-8"),
+            "detail_desc": unk.encode("utf-8")}
 
 def load_articles_ds():
     articles_fn = os.path.abspath(os.path.join(
@@ -49,14 +51,16 @@ def load_articles_ds():
             "detail_desc",
             ]
     articles_ds = pd.read_csv(articles_fn)
-    articles_ds["detail_desc"] = articles_ds["detail_desc"].fillna("")
-    articles_ds["article_id"] = articles_ds["article_id"].astype(str)
+    articles_ds["article_id"] = (articles_ds["article_id"]
+            .apply(lambda x: f"{x:010d}"))
     for col in articles_bytes_cols:
         articles_ds[col] = (
-            articles_ds[col].str.casefold()
+            articles_ds[col]
+                .fillna(unk)
+                .str.casefold()
                 .str.encode("utf-8"))
     articles_ds = articles_ds.set_index("article_id")
-    articles_ds.loc[b"0"] = get_unknown_article()
+    articles_ds.loc[b""] = get_unknown_article()
     return articles_ds
 
 def load_customers_ds():
@@ -69,11 +73,28 @@ def load_customers_ds():
             "fashion_news_frequency",]
     customers_ds = pd.read_csv(customer_fn)
     customers_ds["fashion_news_frequency"] = (
-            customers_ds["fashion_news_frequency"].fillna(""))
+            customers_ds["fashion_news_frequency"]
+                .fillna(unk)
+                .str.casefold())
     customers_ds["club_member_status"] = (
-            customers_ds["club_member_status"].fillna(""))
+            customers_ds["club_member_status"]
+                .fillna(unk)
+                .str.casefold())
     for col in customers_bytes_cols:
-        customers_ds[col] = customers_ds[col].str.encode("utf-8")
+        customers_ds[col] = (customers_ds[col]
+                             .str.encode("utf-8"))
+    idx = pd.notnull(customers_ds["FN"])
+    customers_ds.loc[idx, "fn_mask"] = 1.0
+    customers_ds.loc[~idx, "fn_mask"] = 0.0
+    customers_ds["FN"] = customers_ds["FN"].fillna(0.0)
+    idx = pd.notnull(customers_ds["Active"])
+    customers_ds.loc[idx, "active_mask"] = 1.0
+    customers_ds.loc[~idx, "active_mask"] = 0.0
+    customers_ds["Active"] = customers_ds["Active"].fillna(0.0)
+    idx = pd.notnull(customers_ds["age"])
+    customers_ds.loc[idx, "age_mask"] = 1.0
+    customers_ds.loc[~idx, "age_mask"] = 0.0
+    customers_ds["age"] = customers_ds["age"].fillna(0.0)
     customers_ds.set_index("customer_id", inplace=True)
     return customers_ds
 
@@ -84,15 +105,13 @@ def append_previous_purchases(
     article_id = "article_id"
     price = "price"
     sales_channel_id = "sales_channel_id"
-    transaction_ds.sort_values("t_dat", inplace=True)
     for n in range(1, window + 1):
         transaction_ds[f"{article_id}_{n}"] = (
                 transaction_ds
                     .groupby("customer_id")
                     [article_id]
                     .shift(n)
-                    .fillna(b"0")
-                    )
+                    .fillna(b""))
         transaction_ds[f"{price}_{n}"] = (
                 transaction_ds
                     .groupby("customer_id")
@@ -103,17 +122,120 @@ def append_previous_purchases(
                     .groupby("customer_id")
                     [sales_channel_id]
                     .shift(n)
-                    .fillna(0)
-                    .astype(int))
+                    .fillna(unk.encode("utf-8")))
+    for n in range(1, window + 1):
+        transaction_ds[f"{price}_{n}_mask"] = (
+            pd.notnull(transaction_ds[f"{price}_{n}"]))
+        transaction_ds[f"{price}_{n}"] = (
+            transaction_ds[f"{price}_{n}"].fillna(0.0))
     return transaction_ds
 
 def load_transactions_ds():
     transact_fn = os.path.abspath(os.path.join(
             __file__, os.pardir,
             "data", "transactions_train.csv"))
-    transactions_ds = pd.read_csv(transact_fn, nrows=1000)
-    transactions_ds["article_id"] = transactions_ds["article_id"].astype(str)
+    transactions_ds = pd.read_csv(transact_fn, nrows=1000000)
     transactions_ds["article_id"] = (
-            transactions_ds["article_id"].str.encode("utf-8"))
+            transactions_ds["article_id"]
+                .apply(lambda x: f"{x:010d}")
+                .str.encode("utf-8"))
+    transactions_ds["t_dat"] = transactions_ds["t_dat"].str.encode("utf-8")
+    transactions_ds["customer_id"] = (
+            transactions_ds["customer_id"].str.encode("utf-8"))
+    transactions_ds["sales_channel_id"] = (
+            transactions_ds["sales_channel_id"]
+                .apply(lambda x: str(x).encode("utf-8")))
     transactions_ds = append_previous_purchases(transactions_ds)
+    print("columns", transactions_ds.columns)
     return transactions_ds
+
+def write_vocabulary(
+        articles_ds: pd.DataFrame,
+        customers_ds: pd.DataFrame,
+        parent_dir: str="./vocabulary"):
+    """
+    """
+    if not os.path.exists(parent_dir):
+        os.mkdir(parent_dir)
+    article_ids = articles_ds.index.unique().tolist()
+    garment_voc = articles_ds["garment_group_name"].unique().tolist()
+    section_voc = articles_ds["section_name"].unique().tolist()
+    index_voc = articles_ds["index_name"].unique().tolist()
+    index_group_voc = articles_ds["index_group_name"].unique().tolist()
+    department_voc = articles_ds["department_name"].unique().tolist()
+    colour_value_voc = (articles_ds["perceived_colour_value_name"]
+            .unique().tolist())
+    colour_master_voc = (articles_ds["perceived_colour_master_name"]
+            .unique().tolist())
+    colour_group_voc = articles_ds["colour_group_name"].unique().tolist()
+    graphical_appearance_voc = (articles_ds["graphical_appearance_name"]
+            .unique().tolist())
+    product_group_voc = (articles_ds["product_group_name"]
+            .unique().tolist())
+    product_type_voc = articles_ds["product_type_name"].unique().tolist()
+    club_member_voc = customers_ds["club_member_status"].unique().tolist()
+    fashion_news_voc = (customers_ds["fashion_news_frequency"]
+            .unique().tolist())
+    pairs = [
+        (article_ids, "article_ids.txt"),
+        (garment_voc, "garments.txt"),
+        (section_voc, "sections.txt"),
+        (index_voc, "index_names.txt"),
+        (index_group_voc, "index_groups.txt"),
+        (department_voc, "departments.txt"),
+        (colour_value_voc, "colour_value_names.txt"),
+        (colour_master_voc, "colour_master_names.txt"),
+        (colour_group_voc, "colour_group_names.txt"),
+        (graphical_appearance_voc, "graphical_appearance_names.txt"),
+        (product_group_voc, "product_group_names.txt"),
+        (product_type_voc, "product_type_names.txt"),
+        (club_member_voc, "club_member_statuses.txt"),
+        (fashion_news_voc, "fashion_news.txt"),
+    ]
+    for lst, bn in pairs:
+        fn = os.path.join(parent_dir, bn)
+        with open(fn, "w") as f:
+            for w in lst:
+                f.write(w.decode("utf-8") + "\n")
+
+def load_vocabulary(
+        parent_dir: str="./vocabulary"):
+    """
+    """
+    vocabularies = {
+        "article_ids": [],
+        "garment_voc": [],
+        "section_voc": [],
+        "index_voc": [],
+        "index_group_voc": [],
+        "department_voc": [],
+        "colour_value_voc": [],
+        "colour_master_voc": [],
+        "colour_group_voc": [],
+        "graphical_appearance_voc": [],
+        "product_group_voc": [],
+        "product_type_voc": [],
+        "club_member_voc": [],
+        "fashion_news_voc": [],}
+    pairs = [
+        ("article_ids", "article_ids.txt"),
+        ("garment_voc", "garments.txt"),
+        ("section_voc", "sections.txt"),
+        ("index_voc", "index_names.txt"),
+        ("index_group_voc", "index_groups.txt"),
+        ("department_voc", "departments.txt"),
+        ("colour_value_voc", "colour_value_names.txt"),
+        ("colour_master_voc", "colour_master_names.txt"),
+        ("colour_group_voc", "colour_group_names.txt"),
+        ("graphical_appearance_voc", "graphical_appearance_names.txt"),
+        ("product_group_voc", "product_group_names.txt"),
+        ("product_type_voc", "product_type_names.txt"),
+        ("club_member_voc", "club_member_statuses.txt"),
+        ("fashion_news_voc", "fashion_news.txt"),
+    ]
+    for lst, bn in pairs:
+        fn = os.path.join(parent_dir, bn)
+        with open(fn, "r") as f:
+            vocabularies[lst] = [w.strip().encode("utf-8") for w in f]
+    return vocabularies
+
--- hm_recommendation/serialize.py
+++ hm_recommendation/serialize.py
@@ -1,12 +1,14 @@
+import sys
+import os
 from typing import Dict
 import tensorflow as tf
+import pandas as pd
 import numpy as np
 
 def _byteslist(value):
     return tf.train.Feature(
             bytes_list=tf.train.BytesList(value=value))
-def _int64list(value):
-    return tf.train.Feature(
+def _int64list(value): return tf.train.Feature(
             int64_list=tf.train.Int64List(value=value))
 def _floatlist(value):
     return tf.train.Feature(
@@ -18,14 +20,18 @@ def serialize_example(ds: Dict[str, np.array]):
         "customer_id": _byteslist(ds["customer_id"]),
         "fn": _floatlist(ds["FN"]),
         "active": _floatlist(ds["Active"]),
-        "club_member_status": _byteslist(ds["club_member_status"]),
-        "fashion_news_frequency": _byteslist(ds["fashion_news_frequency"]),
         "age": _floatlist(ds["age"]),
         "price": _floatlist(ds["price"]),
-        "sales_channel_id": _int64list(ds["sales_channel_id"]),
+        "club_member_status":
+                _byteslist(ds["club_member_status"]),
+        "fashion_news_frequency":
+                _byteslist(ds["fashion_news_frequency"]),
+        "sales_channel_id": _byteslist(ds["sales_channel_id"]),
         "article_id": _byteslist(ds["article_id"]),
-        "product_type_name": _byteslist(ds["product_type_name"]),
-        "product_group_name": _byteslist(ds["product_group_name"]),
+        "product_type_name":
+                _byteslist(ds["product_type_name"]),
+        "product_group_name":
+                _byteslist(ds["product_group_name"]),
         "graphical_appearance_name":
                 _byteslist(ds["graphical_appearance_name"]),
         "colour_group_name":
@@ -44,9 +50,11 @@ def serialize_example(ds: Dict[str, np.array]):
                 _byteslist(ds["section_name"]),
         "garment_group_name":
                 _byteslist(ds["garment_group_name"]),
-        "detail_desc":
-                _byteslist(ds["detail_desc"]),
-        "target": _byteslist(ds["target"]),}
+        #"detail_desc":
+        #        _byteslist(ds["detail_desc"]),
+        "target_article_id":
+                _byteslist(ds["target_article_id"]),
+        "target": _int64list(ds["target"]),}
     example_proto = tf.train.Example(
             features=tf.train.Features(feature=feature))
     return example_proto.SerializeToString()
@@ -63,32 +71,142 @@ def parse(example):
     feature_description = {
         "t_dat": tf.io.FixedLenFeature([1], tf.string),
         "customer_id": tf.io.FixedLenFeature([1], tf.string),
-        "fn": tf.io.FixedLenFeature([1], tf.float32),
-        "active": tf.io.FixedLenFeature([1], tf.float32),
-        "club_member_status": tf.io.FixedLenFeature([1], tf.string),
-        "fashion_news_frequency": tf.io.FixedLenFeature([1], tf.string),
-        "age": tf.io.FixedLenFeature([1], tf.float32),
-        "price": tf.io.FixedLenFeature([ts_len], tf.float32),
-        "sales_channel_id": tf.io.FixedLenFeature([ts_len], tf.int64),
-        "article_id": tf.io.FixedLenFeature([ts_len], tf.string),
-        "product_type_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "product_group_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "graphical_appearance_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "colour_group_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "perceived_colour_value_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "perceived_colour_master_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "department_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "index_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "index_group_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "section_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "garment_group_name": tf.io.FixedLenFeature([ts_len], tf.string),
-        "detail_desc": tf.io.FixedLenFeature([ts_len], tf.string),
-        "target": tf.io.FixedLenFeature([1], tf.string)}
+        "fn": tf.io.FixedLenFeature([2], tf.float32),
+        "active": tf.io.FixedLenFeature([2], tf.float32),
+        "age": tf.io.FixedLenFeature([2], tf.float32),
+        "price": tf.io.FixedLenFeature([2 * ts_len], tf.float32),
+        "club_member_status":
+                tf.io.FixedLenFeature([1], tf.string),
+        "fashion_news_frequency":
+                tf.io.FixedLenFeature([1], tf.string),
+        "sales_channel_id":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "article_id":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "product_type_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "product_group_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "graphical_appearance_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "colour_group_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "perceived_colour_value_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "perceived_colour_master_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "department_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "index_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "index_group_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "section_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        "garment_group_name":
+                tf.io.FixedLenFeature([ts_len], tf.string),
+        #"detail_desc": tf.io.FixedLenFeature([ts_len], tf.string),
+        "target_article_id":
+                tf.io.FixedLenFeature([1], tf.string),
+        "target": tf.io.FixedLenFeature([1], tf.int64),}
     return tf.io.parse_single_example(example, feature_description)
 
-def serialize():
+def write_chunk(
+            transactions_ds: pd.DataFrame,
+            articles_ds: pd.DataFrame,
+            tfrec_fn: str,
+            tfrec_dir: str="tfrec",
+            ts_len: int=5):
+    dataset = []
+    article_columns = [
+            "product_code",
+            "prod_name",
+            "product_type_no",
+            "product_type_name",
+            "product_group_name",
+            "graphical_appearance_no",
+            "graphical_appearance_name",
+            "colour_group_code",
+            "colour_group_name",
+            "perceived_colour_value_id",
+            "perceived_colour_value_name",
+            "perceived_colour_master_id",
+            "perceived_colour_master_name",
+            "department_no",
+            "department_name",
+            "index_code",
+            "index_name",
+            "index_group_no",
+            "index_group_name",
+            "section_no",
+            "section_name",
+            "garment_group_no",
+            "garment_group_name",
+            "detail_desc"]
+    for i in range(1, ts_len + 1):
+        transactions_ds = transactions_ds.join(
+                articles_ds,
+                on=f"article_id_{i}",
+                how="left",
+                rsuffix=f"_{i}")
+        transactions_ds.rename(
+                {col: f"{col}_{i}"
+                 for col in article_columns},
+                axis=1,
+                inplace=True)
+    out_fp = os.path.join(tfrec_dir, tfrec_fn)
     with tf.io.TFRecordWriter(out_fp) as writer:
-        writer.write(tf_serialize_example(example))
+        for i, row in transactions_ds.iterrows():
+            data = {}
+            data["article_id"] = [
+                    row[f"article_id_{n}"]
+                    for n in range(ts_len, 0, -1)]
+            for col in article_columns:
+                data[col] = [row[f"{col}_{n}"] for n in range(ts_len, 0, -1)]
+            data["t_dat"] = [row["t_dat"]]
+            data["price"] = (
+                    [row[f"price_{n}"]
+                    for n in range(ts_len, 0, -1)]
+                    + [row[f"price_{n}_mask"]
+                    for n in range(ts_len, 0, -1)])
+            data["sales_channel_id"] = [row[f"sales_channel_id_{n}"]
+                    for n in range(ts_len, 0, -1)]
+            data["target"] = [row["target"]]
+            data["target_article_id"] = [row["article_id"]]
+            cust_idx = row["customer_id"]
+            data["customer_id"] = [cust_idx]
+            data["FN"] = [row["FN"], row["fn_mask"]]
+            data["Active"] = [row["Active"], row["active_mask"]]
+            data["club_member_status"] = [row["club_member_status"]]
+            data["fashion_news_frequency"] = [row["fashion_news_frequency"]]
+            data["age"] = [row["age"], row["age_mask"]]
+            writer.write(serialize_example(data))
+
+def write_dataset(
+            transactions_ds: pd.DataFrame,
+            articles_ds: pd.DataFrame,
+            customers_ds: pd.DataFrame,
+            tfrec_dir: str="tfrec",
+            ts_len: int=5,
+            filesize: int=1000000):
+    if not os.path.exists(tfrec_dir):
+        os.mkdir(tfrec_dir)
+    n = len(transactions_ds)
+    shards = n // filesize + 1 if n % filesize != 0 else n // filesize
+    transactions_ds = transactions_ds.join(
+            customers_ds,
+            on="customer_id",
+            how="left")
+    for i in range(shards):
+        idx = range(i * filesize, (i + 1) * filesize)
+        chunk = pd.DataFrame(transactions_ds.iloc[idx])
+        write_chunk(
+                chunk,
+                articles_ds,
+                tfrec_dir=tfrec_dir,
+                ts_len=ts_len,
+                tfrec_fn=f"{i:03d}.tfrec")
+
 
 
 
